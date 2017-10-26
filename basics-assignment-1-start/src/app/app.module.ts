4253d1e27c4d659d9f77e2cce1983df5022c01c1
jmart210 <crusader11jm@yahoo.com>
2017-10-26 14:57:37 -0500

    creating components

---
+++ basics-assignment-1-start/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { WarningalertComponent } from './warningalert/warningalert.component';
+import { SuccessalertComponent } from './successalert/successalert.component';
+import { ChillalertComponent } from './chillalert/chillalert.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WarningalertComponent,
+    SuccessalertComponent,
+    ChillalertComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ basics-assignment-1-start/src/app/chillalert/chillalert.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+ 
+@Component({
+    selector: 'app-chillalert',
+    template: `
+        <p>Dude just chill, your good.</p>
+    `,
+    styles: [
+        `
+            p {
+                padding: 20px;
+                background-color: lightblue;
+                border: 1px solid blue;
+            }
+        `
+    ]
+})
+export class ChillalertComponent {
+
+}
